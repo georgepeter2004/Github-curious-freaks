d480316fba5cde62e2d61a98bf796eb826c03b20
georgepeter2004 <georgepeter200424@gmail.com>
2025-08-06 13:11:30 +0530

    uploading basic code

---
+++ src/CountdigitNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CountdigitNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        int i;
+        for(i = 0;num!=0;i++){
+            num /= 10;
+        }
+
+        System.out.println(i);
+
+
+    }
+}
